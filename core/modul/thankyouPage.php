c1b3ed83652fc90f56d19a34fedc6fd8faae5695
flandy12 <halloflandy@gmail.com>
2021-12-10 17:29:36 +0700

    Update 10 Dec - Mengedit css thankyouPage #1

--- core/modul/thankyouPage.php
+++ core/modul/thankyouPage.php
@@ -6,54 +6,6 @@
             <img src="images/thankyou.png">
         </div>
 
-        <div class="checkout">
-            <div class="container">
-                <div class="row">
-
-                    <!-- QR Code -->
-                    <div class="col-lg-6"> <!-- Coloum 1 -->
-                        <img src="images/items/QR Code.jpg">
-                    
-                    </div> <!-- End of Coloum 1 -->
-
-                    <!-- Order Info -->
-
-                    <div class="col-lg-6"> <!-- Coloum 2 -->
-                        <div class="order checkout_section">
-                            <div class="section_title">Your order</div>
-                            <div class="section_subtitle">Order details</div>
-
-                            <!-- Order details -->
-                            <div class="order_list_container">
-                                <div class="order_list_bar d-flex flex-row align-items-center justify-content-start">
-                                    <div class="order_list_title">Product</div>
-                                    <div class="order_list_value ml-auto">Total</div>
-                                </div>
-                                <ul class="order_list">
-                                    <li class="d-flex flex-row align-items-center justify-content-start">
-                                        <div class="order_list_title">Apple Iphone 11 Pro - 64Gb</div>
-                                        <div class="order_list_value ml-auto">Rp. 14.500.000</div>
-                                    </li>
-                                    <li class="d-flex flex-row align-items-center justify-content-start">
-                                        <div class="order_list_title">Subtotal</div>
-                                        <div class="order_list_value ml-auto">Rp. 14.500.000</div>
-                                    </li>
-                                    <li class="d-flex flex-row align-items-center justify-content-start">
-                                        <div class="order_list_title">Total</div>
-                                        <div class="order_list_value ml-auto">Rp. 14.500.000</div>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <!-- Order Text -->
-                            <div class="order_text">&nbsp;<br><br><br><br></div>
-                        </div>
-                    </div> <!-- End of Coloum 2 -->
-
-                </div>
-            </div>
-        </div>
-
         <!-- <div class="gamethankyou">
             <script src="https://cdn.htmlgames.com/embed.js?game=Avoider&amp;bgcolor=white"></script>
         </div> -->
@@ -61,5 +13,54 @@
 </div>
 
 <?php
-    echo"<meta http-equiv='refresh' content='10; url=https://t3ptg.app.goo.gl/JC6Z9eJLsFJcKepy7'>";
+    
+//     <div class="checkout">
+//     <div class="container">
+//         <div class="row">
+
+//             <!-- QR Code -->
+//             <div class="col-lg-6"> <!-- Coloum 1 -->
+//                 <img src="images/items/QR Code.jpg">
+            
+//             </div> <!-- End of Coloum 1 -->
+
+//             <!-- Order Info -->
+
+//             <div class="col-lg-6"> <!-- Coloum 2 -->
+//                 <div class="order checkout_section">
+//                     <div class="section_title">Your order</div>
+//                     <div class="section_subtitle">Order details</div>
+
+//                     <!-- Order details -->
+//                     <div class="order_list_container">
+//                         <div class="order_list_bar d-flex flex-row align-items-center justify-content-start">
+//                             <div class="order_list_title">Product</div>
+//                             <div class="order_list_value ml-auto">Total</div>
+//                         </div>
+//                         <ul class="order_list">
+//                             <li class="d-flex flex-row align-items-center justify-content-start">
+//                                 <div class="order_list_title">Apple Iphone 11 Pro - 64Gb</div>
+//                                 <div class="order_list_value ml-auto">Rp. 14.500.000</div>
+//                             </li>
+//                             <li class="d-flex flex-row align-items-center justify-content-start">
+//                                 <div class="order_list_title">Subtotal</div>
+//                                 <div class="order_list_value ml-auto">Rp. 14.500.000</div>
+//                             </li>
+//                             <li class="d-flex flex-row align-items-center justify-content-start">
+//                                 <div class="order_list_title">Total</div>
+//                                 <div class="order_list_value ml-auto">Rp. 14.500.000</div>
+//                             </li>
+//                         </ul>
+//                     </div>
+
+//                     <!-- Order Text -->
+//                     <div class="order_text">&nbsp;<br><br><br><br></div>
+//                 </div>
+//             </div> <!-- End of Coloum 2 -->
+
+//         </div>
+//     </div>
+// </div>
+
+    // echo"<meta http-equiv='refresh' content='10; url=https://t3ptg.app.goo.gl/JC6Z9eJLsFJcKepy7'>";
 ?>
