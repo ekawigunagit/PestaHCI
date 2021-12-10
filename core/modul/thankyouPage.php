b71b5c630c23021e646326435eedc90f93998d97
flandy12 <halloflandy@gmail.com>
2021-12-10 18:59:18 +0700

    Update 10 Dec - Mengedit css thankyouPage #4

--- core/modul/thankyouPage.php
+++ core/modul/thankyouPage.php
@@ -13,8 +13,9 @@
                     <!-- QR Code -->
                     <div class="col-lg-6 QR_Scan">
                         <!-- Coloum 1 -->
-                        <img src="images/items/QR Code.jpg">
-
+                        <div class="card">
+                             <img src="images/items/QR Code.jpg" >
+                        </div>
                     </div> <!-- End of Coloum 1 -->
 
                     <!-- Order Info -->
