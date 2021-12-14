4e8fdfe4a2d67e59cca7199202ad4b7d0a438aa0
ekawigunagit <ekawigunawork@gmail.com>
2021-12-14 11:49:06 +0700

    Update Tgl 14 - Update Promo ASC

--- source.php
+++ source.php
@@ -52,7 +52,7 @@
                 <div class="swiper-wrapper">
                     <!-- Slides -->
                     <?php
-                    $data_promohci = "SELECT * FROM promos WHERE status=1 ORDER BY id DESC";
+                    $data_promohci = "SELECT * FROM promos WHERE status=1 ORDER BY id ASC";
                     $query_promohci = mysqli_query($koneksi, $data_promohci);
 
                     while ($show_promohci = mysqli_fetch_array($query_promohci)) {
