86a06b973560f699b314efeab5bf106ac7011094
flandy12 <halloflandy@gmail.com>
2022-01-18 15:41:00 +0700

    Update - Tgl 18 Jan update game#1

--- core/modul/gimmickPage.php
+++ core/modul/gimmickPage.php
@@ -1,9 +1,8 @@
-<br /><br /><br /><br /><br />
+
 <div class="container">
-    <div class="row justify-content-center mt-5">
+    <div class="row justify-content-center gimmick">
         <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" id="game" src="http://localhost/hciproject/pestaHCI/GameHSJ/"></iframe>
+            <iframe class="embed-responsive-item" id="game" src="http://localhost/PestaHCI1/pestaHCI/GameHSJ/"></iframe>
         </div>
     </div>
-</div>
 </div>
