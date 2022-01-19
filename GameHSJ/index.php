df8dd6e57466db102019f1afe30bda2b431cb335
ekawigunagit <ekawigunawork@gmail.com>
2022-01-19 17:06:59 +0700

    Update Tgl 19 jan - Update Game 6

--- GameHSJ/index.php
+++ GameHSJ/index.php
@@ -1,32 +1,52 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Hurry Space Jam</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover">
 
-<meta name="generator" content="Construct 3">
+<head>
+	<meta charset="UTF-8">
+	<title>Hurry Space Jam</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover">
+
+	<meta name="generator" content="Construct 3">
 
 	<link rel="manifest" href="appmanifest.json">
 	<link rel="apple-touch-icon" sizes="180x170" href="icons/icon-16.png">
 	<link rel="icon" type="image/png" href="icons/icon-16.png">
 	<style>
-		@media(max-width:576px){
+		/* @media(min-width:1024px) {
 			canvas {
-			overflow: none !important;
-			width: 100% !important; 
-			height:100% !important;
-			padding:0 !important;
-			margin: 0 !important;
+				overflow: none !important;
+				height: 100% !important;
+				padding: 0 !important;
+				margin: 0 !important;
+			}
 		}
+
+		@media(max-width:576px) {
+			canvas {
+				overflow: none !important;
+				width: 100% !important;
+				height: 100% !important;
+				padding: 0 !important;
+				margin: 0 !important;
+			}
 		}
+
+		@media(max-width:576px) {
+			input {
+				position: absolute !important;
+				top: 165px !important;
+				padding: 0 !important;
+				margin: 0 !important;
+			}
+		} */
 	</style>
 
 
 </head>
+
 <body>
 
-<div id="fb-root"></div>
+	<div id="fb-root"></div>
 
 	<noscript>
 		<div id="notSupportedWrap">
@@ -34,9 +54,10 @@
 			<p class="notSupportedMessage">JavaScript appears to be disabled. Please enable it to view this content.</p>
 		</div>
 	</noscript>
-	<script src="scripts/supportcheck.js?t=<?php echo date("S") ;?>"></script>
-	<script src="scripts/offlineclient.js?t=<?php echo date("S") ;?>" type="module"></script>
-	<script src="scripts/main.js?t=<?php echo date("S") ;?>" type="module"></script>
-	<script src="scripts/register-sw.js?t=<?php echo date("S") ;?>" type="module"></script>
+	<script src="scripts/supportcheck.js?t=<?php echo date("S"); ?>"></script>
+	<script src="scripts/offlineclient.js?t=<?php echo date("S"); ?>" type="module"></script>
+	<script src="scripts/main.js?t=<?php echo date("S"); ?>" type="module"></script>
+	<script src="scripts/register-sw.js?t=<?php echo date("S"); ?>" type="module"></script>
 </body>
+
 </html>
--- core/modul/gimmickPage.php
+++ core/modul/gimmickPage.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="gimmick">
        
-            <iframe id="game" src="http://localhost/PestaHCI1/pestaHCI/GameHSJ/?t=<?php echo date('s'); ?>"></iframe>
+            <iframe id="game" src="http://localhost/hciproject/pestaHCI/GameHSJ/?t=<?php echo date('s'); ?>"></iframe>
         
     </div>
 </div>
