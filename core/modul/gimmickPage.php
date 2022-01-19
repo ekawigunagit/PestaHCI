2b8a80818be8978f259564ac18c14e3b3a2174ec
flandy12 <halloflandy@gmail.com>
2022-01-19 15:13:38 +0700

    update 19 --mengedit our promo, hadiah + game#4

--- core/modul/gimmickPage.php
+++ core/modul/gimmickPage.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="gimmick">
-        <div class="embed-responsive embed-responsive-16by9 ">
-            <iframe class="embed-responsive-item" id="game" src="http://localhost/PestaHCI1/pestaHCI/GameHSJ/?t=<?php echo date('s'); ?>"></iframe>
-        </div>
+       
+            <iframe id="game" src="http://localhost/PestaHCI1/pestaHCI/GameHSJ/?t=<?php echo date('s'); ?>"></iframe>
+        
     </div>
 </div>
